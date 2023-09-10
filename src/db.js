937b816544b329c7d5233dfcb5e423b2b4e712a4
juan david iron-mind <davidtovarmontoya@gmail.com>
2023-09-09 23:22:38 -0500

    db variable fix fl0

--- src/db.js
+++ src/db.js
@@ -1,16 +1,16 @@
-import {Sequelize} from 'sequelize';
-import config from '../lib/config.js';
-console.log(config.dbUri)
+import { Sequelize } from "sequelize";
+import config from "../lib/config.js";
+console.log(config.dbUri);
 //coneccion local
-const sslConfig = process.env.CONNECTION=="local"?"":"?sslmode=no-verify"
-const sequelize = new Sequelize(`${config.dbUri}${sslConfig}`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions : process.env.CONNECTION=="local"|| {
-      ssl : {
-        require: !(process.env.CONNECTION=="local"),
-        rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-      }
-    }
-})
+// const sslConfig = process.env.CONNECTION=="local"?"":"?sslmode=no-verify"
+const sequelize = new Sequelize(`${config.dbUri}`, {
+	logging: false, // set to console.log to see the raw SQL queries
+	native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+	// dialectOptions: process.env.CONNECTION == "local" || {
+	// 	ssl: {
+	// 		require: !(process.env.CONNECTION == "local"),
+	// 		rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+	// 	},
+	// },
+});
 export default sequelize;
